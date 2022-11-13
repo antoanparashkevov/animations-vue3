022b0d22578b4ec39b236d2305b8078d84384c27
“antoanparashkevov” <tonkata1505@gmail.com>
2022-11-13 09:37:12 +0200

    When the element is unmounted to the DOM to animate it

--- src/App.vue
+++ src/App.vue
@@ -126,5 +126,17 @@ button:active {
     transform: translateY(0);/*real position it should have on this page*/
 }
 
+.v-leave-from {
+    opacity: 1;
+    transform: translateY(0);
+}
 
+.v-leave-active {
+    transition: all 0.3s ease-in;
+}
+
+.v-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
+}
 </style>
