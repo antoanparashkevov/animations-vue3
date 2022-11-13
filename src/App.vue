b7f3e16530644ea70ca22e6c5e0ac1e0ad706a1a
“antoanparashkevov” <tonkata1505@gmail.com>
2022-11-13 09:51:51 +0200

    Basic modal animation with transition

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
         </transition>
         <button @click="togglePara">Toggle para</button>
     </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
